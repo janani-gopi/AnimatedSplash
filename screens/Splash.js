b2a3ee04beeee1eac2c34be863d7d59bb279b60b
janani-gopi <jananirave19@gmail.com>
2023-08-31 16:57:40 +0530

    fourth commit:Splash screen:added image animation

--- screens/Splash.js
+++ screens/Splash.js
@@ -1,9 +1,10 @@
 import {useState} from 'react';
-import {Animated, Image} from 'react-native';
+import {Animated, Image, Dimensions} from 'react-native';
 import BootSplash from 'react-native-bootsplash';
 
 export const Splash = ({onAnimationEnd}) => {
   const [opacity] = useState(() => new Animated.Value(1));
+  const [translateY] = useState(() => new Animated.Value(0));
 
   const {container, logo /*, brand */} = BootSplash.useHideAnimation({
     manifest: require('../assets/bootsplash_manifest.json'),
@@ -13,11 +14,27 @@ export const Splash = ({onAnimationEnd}) => {
     navigationBarTranslucent: false,
 
     animate: () => {
+      //translate Y animation
+      const {height} = Dimensions.get('window');
+
+      Animated.stagger(150, [
+        Animated.spring(translateY, {
+          useNativeDriver: true,
+          toValue: -50,
+          delay:3000
+        }),
+        Animated.spring(translateY, {
+          useNativeDriver: true,
+          toValue: height,
+        }),
+      ]).start();
+
       // Perform animations and call onAnimationEnd
       Animated.timing(opacity, {
         useNativeDriver: true,
         toValue: 0,
         duration: 400,
+        delay:500
       }).start(() => {
         onAnimationEnd();
       });
@@ -26,7 +43,10 @@ export const Splash = ({onAnimationEnd}) => {
 
   return (
     <Animated.View {...container} style={[container.style, {opacity}]}>
-      <Image {...logo} style={logo.style} />
+       <Animated.Image
+        {...logo}
+        style={[logo.style, { transform: [{ translateY }] }]}
+      />
     </Animated.View>
   );
 };
